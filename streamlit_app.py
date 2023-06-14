501884872e53023e05413e9a8bd3a02832ef2541
kharichandana <118801165+kharichandana@users.noreply.github.com>
2023-06-14 16:16:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,8 @@ try:
        back_from_function = get_fruityvice_data(fruit_choice)
        streamlit.dataframe(back_from_function)
       
-
+except URLError as e:
+   streamlit.error()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
