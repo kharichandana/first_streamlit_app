426b5d213327ac57ce695b3341728aff7f167ba2
kharichandana <118801165+kharichandana@users.noreply.github.com>
2023-06-14 16:57:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ if streamlit.write('Add a Fruit to the List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    back_from_function = insert_row_snowflake(add_my_fruit)
    streamlit.text(back_from_function)
-   my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+   my_cnx.execute("insert into fruit_load_list values ('from streamlit')")
 
 
 
